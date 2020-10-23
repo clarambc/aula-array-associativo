96baad1261488b9be3d0542bb5423b6fab03983c
Ana <clarambc@gmail.com>
2020-10-22 22:28:25 -0300

    exercicios de array associativo

---
+++ array-associativo-exercicios.php
@@ -0,0 +1,8 @@
+<?php
+
+$listaNumero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for ($i = 1; $i < count($listaNumero); $i++) {
+    echo $listaNumero [$i] . PHP_EOL;
+}
+
---
+++ array-associativo.php
@@ -0,0 +1,8 @@
+<?php
+    $apto1 = [
+        601 => 'Ana',
+        'titular' => 'Ana',
+        'Aluguel' =>  2000
+    ];
+
+    echo $apto1 [601];
